130b718c9870bea84a31516989f73e46d10868e6
Scott Hill <scott.c.hill.jr@gmail.com>
2022-01-02 16:36:26 -0600

    commit tree 67e08f899f3176fd95606369c454e6cfc240caa1

--- index.js
+++ index.js
@@ -1,11 +1,23 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(height){
+  if(height <= 400) {
+    return 'This one is on me!';
+  }
+  else if(height > 2000 && height <= 2500) {
+    return 'I will gladly take your thirty bucks.';
+  }
+  else if(height > 2500) {
+    return 'No can do.';
+  }
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
+function ternaryCheckCity(city){
+  return(city==='NYC'?"Ok, sounds good.":"No go.");
 }
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+function switchOnCharmFromTip(generosity){
+  switch(generosity) {
+    case 'generous': return "Thank you so much.";
+    case 'not as generous': return "Thank you.";
+    default: return "Bye.";
+  }
 }
